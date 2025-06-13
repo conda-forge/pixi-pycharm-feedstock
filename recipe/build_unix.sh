b4b100f960aba5abab8b07a826af84382b277149
Pavel Zwerschke <pavelzw@gmail.com>
2025-06-13 12:00:46 +0200

    convert to v1 recipe

--- recipe/build_unix.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p $PREFIX/libexec
-echo "#!$PREFIX/bin/python" > $PREFIX/libexec/conda
-cat conda >> $PREFIX/libexec/conda
-sed -i '2d' $PREFIX/libexec/conda
-chmod +x $PREFIX/libexec/conda
--- recipe/build_win.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p $PREFIX/libexec
-cp conda $PREFIX/libexec/conda.py
-sed -i '1d' $PREFIX/libexec/conda.py
-cp conda.bat $PREFIX/libexec/conda.bat
